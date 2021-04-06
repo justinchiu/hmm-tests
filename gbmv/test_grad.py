a79eb2380cd28d298c946c49b07a36b5e943a230
Justin T Chiu <jtc257@graphite-login-01.coecis.cornell.edu>
2021-04-05 22:42:09 -0400

    genbmm different from cublas

--- gbmv/test_grad.py
+++ gbmv/test_grad.py
@@ -6,7 +6,7 @@ device = torch.device("cuda:0")
 #device = torch.device("cpu")
 
 C = 8
-K = 3
+K = 2
 
 K1K = 2*K+1
 
@@ -15,7 +15,7 @@ bA = torch.randn(K1K, C, device=device)
 x = torch.randn(C, device=device)
 
 # dense
-def to_dense(bA):
+def band_to_dense(bA):
     K1K, C = bA.shape
     K = K1K // 2
     A = torch.zeros(C, C, device=device)
@@ -32,27 +32,57 @@ def to_dense(bA):
     A.diagonal().copy_(bA[K])
     return A
 
-A = to_dense(bA)
-y = A.T @ x # x @ A
+def band_to_dense2(bA):
+    K1K, C = bA.shape
+    K = K1K // 2
+    A = torch.zeros(C, C, device=device)
+    for j in range(C):
+        for i in range(max(0, j-K), min(C, j+K)):
+            A[i,j] = bA[K+1+i-j, j]
+    return A
+
+A = band_to_dense(bA)
+#y = A.T @ x # x @ A # really want this though
+y = A @ x
+
+A2 = band_to_dense2(bA)
+y2 = A2 @ x
 
 def clamp(x, l, u):
     return min(u, max(l, x))
 
 # convert to C x K matrix
-def to_rows(bA):
+def band_to_rows(bA):
     K1K, C = bA.shape
-    A = to_dense(bA)
+    A = band_to_dense(bA)
     A_rows = torch.zeros(C, K1K, device=device)
     for c in range(C):
         lower = clamp(c-K, 0, C)
         upper = clamp(c+K+1, 0, C)
         length = upper - lower
-        A_rows[c,:length].copy_(A[c, lower:upper])
+        start = 0 if c >= K else K-c
+        A_rows[c,start:start+length].copy_(A[c, lower:upper])
     return A_rows
 
-A_rows = to_rows(bA)
 xb = genbmm.BandedMatrix(x.view(1, C, 1), 0, 0)
-Ab = genbmm.BandedMatrix(A_rows.view(1, C, K1K), K, K)
+Ab = genbmm.BandedMatrix(bA.view(1, C, K1K), K, K)
 yb = xb.multiply(Ab).data.sum(2).squeeze()
+
+# Ab.to_dense()
+# Ab.data
+
+def rows_to_dense(rA):
+    C, K1K = rA.shape
+    A = torch.zeros(C, C, device=device)
+    for c in range(C):
+        lower = clamp(c-K, 0, C)
+        upper = clamp(c+K+1, 0, C)
+        length = upper - lower
+        start = 0 if c >= K else K-c
+        #A_rows[c,:length].copy_(A[c, lower:upper])
+        A[c,lower:upper] = rA[c,start:start+length]
+    return A
+rA = band_to_rows(bA)
+A3 = rows_to_dense(rA)
 
 import pdb; pdb.set_trace()
